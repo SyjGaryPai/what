3e41988bf9c3b140ddd59aac7328e5437d519275
Gary <gr0983961958@gmail.com>
2019-05-14 10:15:52 +0800

    tallmewhyr33rr3

--- what/ViewController.swift
+++ what/ViewController.swift
@@ -14,7 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func why(_ sender: Any) {
+    }
+    
 
 }
 
